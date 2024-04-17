62a2a97c77c55559997adf1385cad0b6a3d27d7a
YFsanzui <yoshimura.futa.j5@s.mail.nagoya-u.ac.jp>
2024-04-17 10:35:07 +0900

    update setup.py to read requirements.txt

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 # setup.py
-from setuptools import setup; setup()
+from setuptools import setup
+
+def get_requirements():
+    with open("./requirements.txt") as f_in:
+        requirements = f_in.read().splitlines()
+    return requirements
+
+setup(
+    install_requires=get_requirements()
+    )
